4b3cf1d55bb102a62be77b8882d42d9afed5a8a7
Nahuelitos <133977488+Nahuelitos@users.noreply.github.com>
2024-10-01 16:01:00 -0300

    Dejaste tu cuenta abierta

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ec1c9691fef83ccd6ce7a00a468240f2e15cbbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cce2a6e913379eeda6d8793d008e7f7f09d02bc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
